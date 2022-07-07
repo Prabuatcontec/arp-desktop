1ee1ad20d02f2de7d60209c93382709a4f37328c
Prabu M <mprabu@gocontec.com>
2022-07-07 07:41:22 -0400

    Update data config

---
+++ config.py
@@ -0,0 +1,22 @@
+class Config(object):
+    DATABASE_CONFIG = {
+        'server': 'arp.cnvayknczt9m.us-east-1.rds.amazonaws.com',
+        'user': 'admin',
+        'password': 'Suut2FtvXn4869vKzKez',
+        'name': 'Automation',
+    }
+    UPLOAD_FOLDER = './static/uploads/'
+    API_URL = 'http://localhost:5000/api/'
+    API_USER_URL = 'http://api.vulcan.contecprod.com/api/'
+    REPO_PATH = 'https://github.com/Prabuatcontec/ocr-receiving'
+    DEEPBLU_URL = 'https://deepbluapi.gocontec.com'
+    DEEPBLU_KEY = 'QVVUT1JFQ0VJVkU6YXV0b0AxMjM='
+    API_MOTOR_URL = 'http://10.10.145.34:8080/api/'
+    CAMERA_NO = 2
+    CAMERA_NO_TWO = 6
+    CAMERA_WIDTH = 2770
+    CAMERA_HEIGHT = 2770
+    STATION_ID = 'ARPNC1'
+    CAMERA_FRAME_LEN = (900,900)
+    PRINTER_IP = "10.10.145.17"
+
